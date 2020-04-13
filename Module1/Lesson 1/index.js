419c03a859e9b831cb57abe0c07fa84545f9b120
AndriiShnurkov <Shans12.84@ukr.net>
2020-04-13 18:14:03 +0300

    added 1 module lesson 1

---
+++ Module1/Lesson 1/index.js
@@ -0,0 +1,273 @@
+'use strict';
+
+// console.log('Hello JS');
+
+// =================== Number =================
+
+// const age = 30;
+// const pi = 3.14;
+
+// console.log(age + pi);
+
+// =========== String =====================
+
+// const name = 'Andrii'; //'', "", ``
+
+// ==================== Boolean =============
+// true false
+// let isAuthenticated = false;
+// let isLogin = false;
+// isAuth = true;
+// isLogin = false;
+// isAuth && isLogin && console.log('Authenticated');
+
+// ================= null =======================
+// let number = null;
+
+// ================ undefined ===================
+
+// let number;
+// console.log(number);
+
+// console.log(typeof number);
+// console.log(typeof name);
+
+// ========================================================
+// let a = 'Hello';
+// alert(`${a} Java Script`);
+// // console.log(`${a} Java Script`);
+
+// const input = prompt('Введите число'); //string
+
+// const input = confirm('Is you agree with login');
+
+// console.log('input :', input);
+
+// ============================================
+
+// let x = 4;
+// let y = 10;
+
+// console.log(x + y);
+// console.log(y - x);
+// console.log(x / y);
+// console.log(x * y);
+// console.log(y % x);
+
+// x -= 10;
+// x = x - 10;
+
+// x++
+// ++x;
+
+// for (let i = 0; i < 10; ++i) {
+//     // ++i
+//     console.log(i);
+// }
+
+// console.log(x);
+
+// =============================
+
+
+
+// console.log(x >= y); //true
+// > , < , >=, <=,  
+
+// console.log(4 == '4');
+
+// console.log(typeof x);
+// console.log(typeof y);
+
+// console.log(x == y);
+
+// console.log(4 === '4');
+
+// let input = prompt('enter number');
+
+// console.log(input == '4'); //true false
+
+// let x = 4;
+// let y = '4';
+
+// console.log(true === true);
+
+// ===========================
+
+// console.log('fdssdf');
+// alert('dfsdfsd');
+
+// confirm();
+// prompt();
+
+// =======================
+
+// let x = '5';
+// let y = 5;
+
+// // console.log(typeof Number(x));
+
+
+// console.log(x);
+// console.log(y);
+
+// console.log();
+
+// let input = prompt('Enter number');
+
+// console.log(input);
+
+// if (input === null) {
+//     console.log(a + b + c + d);
+// }
+
+// =========== Number =============
+// let x = '5';
+// console.log(Number(x)); //5
+
+
+// ============== parseInt === parseFloat =========
+// let x = '5px';
+
+// // let y = Number.parseInt(x);
+
+// let y = '0.8';
+// let result = Number.parseFloat(y)
+
+// console.log(result);
+
+// =================== NaN ================
+
+// let x = 5;
+
+// console.log(Number.isNaN(x));
+// console.log(Number.isNaN(x)); //true //false
+
+// ==================== Math ===============
+
+// console.log(Math.floor(1.5));
+// console.log(Math.ceil(1.5));
+// console.log(Math.round(1.4));
+
+// let a = 5;
+// let b = 15;
+// let c = 25;
+
+// console.log(Math.min(a, b, c));
+// console.log(Math.max(a, b, c));
+
+// console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+
+// ====================== String =============
+
+// let str1 = 'Lorem ';
+// let str2 = 'ipsum dolor ';
+// let str3 = 'sit amet.';
+
+
+// console.log(str1 + str2 + str3);
+
+// let string = `Don't`;
+
+// let name = 'Andrii';
+// let age = 30;
+
+// let string = `His name is ${name}. He is ${age}`
+
+// console.log(string);
+
+// ====================
+
+// let str = 'Lorem ipsum dolor sitdol.';
+// let str = new String('Lorem ipsum dolor sit.')
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// str.includes('dolor');
+// console.log(str.indexOf('dolor'));
+
+// let name = "Milk"
+
+// const = `<li>${name}</li>`
+
+
+// =========================
+// const message = 'JavaScript is awesome!';
+
+// console.log(message);
+
+// const name = 'Mango';
+
+// console.log('My name is: ', name); // My name is: Mango
+
+
+// alert(message);
+
+let num = '20';
+
+console.log(+num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
